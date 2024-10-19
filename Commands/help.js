019e9e0641c82de7d099425df0ba6dbf97b21f33
Frank kaumba <115949759+efkidgamer@users.noreply.github.com>
2024-10-19 19:16:23 +0200

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -1,1 +1,22 @@
+module.exports = {
+  name: 'help',
+  author: 'Frank Kaumba',
+  requiresAdmin: false,
+  execute: async (bot, from, args) => {
+    const helpMessage = `
+      *Available Commands:*
 
+      • /help - Show this message
+      • /ban - Ban a user
+      • /unban - Unban a user
+      • /mute - Mute a user
+      • /unmute - Unmute a user
+      • /kick - Kick a user
+      • /promote - Promote a user
+
+      *Note:* Use /<command> to execute commands.
+    `;
+
+    bot.sendMessage(from, helpMessage);
+  },
+};
