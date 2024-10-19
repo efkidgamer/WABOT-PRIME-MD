1cb8bb2e02bcea3bfb7a3ac36a15f9b28fd64670
Frank kaumba <115949759+efkidgamer@users.noreply.github.com>
2024-10-19 09:09:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
-.
+//enjoy by Frank kaumba
+const config = {
+  sessionID: 'YOUR_SESSION_ID',
+  phoneNumber: 'YOUR_WHATSAPP_PHONE_NUMBER',
+  verificationCode: 'YOUR_WHATSAPP_VERIFICATION_CODE',
+  prefix: '/', 
+  admin: {
+    numbers: ['1234567890', '9876543210'],
+    // Admin powers
+    powers: ['ban', 'unban', 'mute', 'unmute', 'kick', 'promote'],
+  },
+ owner: '1234567890',
+};
